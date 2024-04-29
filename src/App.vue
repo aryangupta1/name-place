0f30a115ff2bca505f2a4f263cb0e48633a775cb
aryangupta1 <aryangupta1@outlook.com>
2024-04-29 19:18:50 +1000

    push to prod

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-gray-800 text-white flex flex-col items-center py-12">
+  <div class="min-h-screen text-white flex flex-col items-center py-12">
     <header class="text-center mb-12">
       <h1 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
         Name Place
--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="space-y-8">
+  <div class="w-full h-full max-w-screen-xl mx-auto px-4 space-y-8">
     <div v-if="!gameStarted" class="text-center">
       <button @click="startGame" class="px-6 py-2 bg-blue-500 hover:bg-blue-600 rounded shadow">
         Start Game
       </button>
     </div>
-    <div v-else class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+    <div v-else class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-5">
       <div v-for="(input, key) in inputs" :key="key" class="flex flex-col items-center">
         <label :for="key" class="mb-2 font-semibold">{{ key }}</label>
         <input v-model="inputs[key]" :id="key" :placeholder="`Enter a ${key}`"
                class="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
                autocomplete="off"/>
       </div>
-      <button @click="checkEntries" class="col-span-full md:col-span-2 lg:col-span-4 px-6 py-2 bg-green-500 hover:bg-green-600 rounded shadow">
+      <button @click="checkEntries" class="col-span-full sm:col-span-2 lg:col-span-5 px-6 py-2 bg-green-500 hover:bg-green-600 rounded shadow">
         Check Entries
       </button>
     </div>
@@ -27,7 +27,7 @@ const inputs = reactive({
   Place: '',
   Animal: '',
   Thing: '',
-  Food: ''  // Added Food category
+  Food: ''
 });
 const gameStarted = ref(false);
 
